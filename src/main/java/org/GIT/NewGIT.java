f03cdcc81b850a7c07a14245678a51090eee9f50
jacimithun <jacimithun@gmail.com>
2020-05-27 09:57:08 +0530

    Ryan coding is Done

--- src/main/java/org/GIT/NewGIT.java
+++ src/main/java/org/GIT/NewGIT.java
@@ -1,9 +1,50 @@
 package org.GIT;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class NewGIT {
 	
-	public static void main(String[] args) {
-		
+	@Before
+	public void startTime() {
+		System.out.println("Testcase starts");
+	}
+
+	@After
+	public void endTime() {
+		System.out.println("testcase ends");
+	}
+
+	@Test
+	public void tc3() {
+		System.out.println("Test3");
+
+	}
+
+	@BeforeClass
+	public static void launch() {
+		System.out.println("Launch");
+	}
+
+	@AfterClass
+	public static void quit() {
+		System.out.println("close");
+	}
+
+	@Test
+	public void tc2() {
+		System.out.println("Test2");
+
+	}
+
+	@Test
+	public void tc1() {
+		System.out.println("Test1");
+
 	}
 
 }
+	
